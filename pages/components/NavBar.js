294ffa89bf028d52b0f1ef31938afc0fb8de8379
Asarel Castellanos <asarelc@gmail.com>
2022-04-11 18:59:30 -0700

    NavBar completed

--- pages/components/NavBar.js
+++ pages/components/NavBar.js
@@ -7,7 +7,7 @@ import  { MenuIcon, XIcon } from '@heroicons/react/outline'
 
 const NavBar = () => {
   return (
-    <Disclosure as="nav" className="bg-white shadow">
+    <Disclosure as="nav" className="bg-primary">
       {({ open }) => (
         <>
           <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -15,7 +15,7 @@ const NavBar = () => {
               <div className="flex">
                 <div className="-ml-2 mr-2 flex items-center md:hidden">
                   {/* Mobile menu button */}
-                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                  <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                     <span className="sr-only">Open main menu</span>
                     {open ? (
                       <XIcon className="block h-6 w-6" aria-hidden="true" />
@@ -38,22 +38,22 @@ const NavBar = () => {
                   />
                 </div>
                 {/* NavBar Links */}
-                <div className="hidden md:ml-6 md:flex md:space-x-8">
+                <div className="hidden md:ml-6 md:flex md:space-x-2">
                   <a
-                    href="#"
-                    className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                    href="#home"
+                    className="btn btn-ghost text-white hover:border-white inline-flex items-center px-2 mt-2 text-sm font-medium"
                   >
                     Home
                   </a>
                   <a
-                    href="#"
-                    className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                    href="#service"
+                    className="btn btn-ghost text-white hover:border-white inline-flex items-center px-2 mt-2 text-sm font-medium"
                   >
                     Services
                   </a>
                   <a
-                    href="#"
-                    className="border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                    href="#find-us"
+                    className="btn btn-ghost text-white hover:border-white inline-flex items-center px-2 mt-2 text-sm font-medium"
                   >
                     Find Us
                   </a>
@@ -74,22 +74,22 @@ const NavBar = () => {
               {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700" */}
               <Disclosure.Button
                 as="a"
-                href="#"
-                className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+                href="#home"
+                className="btn btn-ghost text-white hover:border-white block pr-4 py-2 text-base font-medium sm:pl-5 sm:pr-6"
               >
                 Home
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
-                href="#"
-                className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+                href="#service"
+                className="btn btn-ghost text-white hover:border-white block pl-3 pr-4 py-2 text-base font-medium sm:pl-5 sm:pr-6"
               >
                 Services
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
-                href="#"
-                className="border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium sm:pl-5 sm:pr-6"
+                href="#find-us"
+                className="btn btn-ghost text-white hover:border-white block pl-3 pr-4 py-2 text-base font-medium sm:pl-5 sm:pr-6"
               >
                 Find Us
               </Disclosure.Button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,34 @@ module.exports = {
   // Adding Custom DaisyUI Configurations
   daisyui: {
     styled: true,
-    themes: true,
+    themes: [
+      {
+        salon: {
+          primary: "#31583B",
+
+          secondary: "#6C8D5E",
+
+          accent: "#687B71",
+
+          neutral: "#193226",
+
+          "base-100": "#9BB0A5",
+
+          info: "#8DCEF8",
+
+          success: "#CEFBE3",
+
+          warning: "#F9F871",
+
+          error: "#F87272",
+        },
+      },
+    ],
     base: true,
     utils: true,
     logs: true,
     rtl: false,
     prefix: "",
-    darkTheme: "dark",
+    darkTheme: "salon",
   },
-}
+};
