a7b6ef4eae31728bbe03d511c2af8248952c9836
Asarel Castellanos <asarelc@gmail.com>
2022-04-11 22:43:58 -0700

    Finished Services

--- pages/components/Services.js
+++ pages/components/Services.js
@@ -4,152 +4,142 @@ const Services = () => {
   return (
     <section className="bg-primary body-font">
       <div className="container px-5 py-24 mx-auto">
-        <div className="text-center mb-20">
-          <h1 className="sm:text-3xl text-2xl font-medium text-center title-font text-white mb-4">
+        <div className="flex flex-col text-center w-full mb-20">
+          <h1 className="sm:text-4xl text-3xl font-medium title-font mb-2 text-white">
             Our Services
           </h1>
-          <p className="text-white text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+          <p className="lg:w-2/3 mx-auto leading-relaxed text-white text-base">
             It is high encouraged to first book a consult so we may discuss
             specifics and so I can also better grasp the nature of your
             appointment. Below are our starting prices. They may vary depeding
             on hair length, volume and texture.
           </p>
         </div>
-        <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-          <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-secondary rounded flex p-4 h-full items-center">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="text-white w-6 h-6 flex-shrink-0 mr-4"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-              >
-                <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
-                <path
-                  fillRule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <span className="text-white title-font font-medium">
-                Authentic Cliche Forage
-              </span>
-            </div>
-          </div>
-          <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-secondary rounded flex p-4 h-full items-center">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="3"
-                className="text-white w-6 h-6 flex-shrink-0 mr-4"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <span className="text-white title-font font-medium">
-                Authentic Cliche Forage
-              </span>
-            </div>
-          </div>
-          <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-secondary rounded flex p-4 h-full items-center">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="3"
-                className="text-white w-6 h-6 flex-shrink-0 mr-4"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <span className="text-white title-font font-medium">
-                Authentic Cliche Forage
-              </span>
-            </div>
-          </div>
-          <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-secondary rounded flex p-4 h-full items-center">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="3"
-                className="text-white w-6 h-6 flex-shrink-0 mr-4"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <span className="text-white title-font font-medium">
-                Authentic Cliche Forage
-              </span>
-            </div>
-          </div>
-          <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-secondary rounded flex p-4 h-full items-center">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="3"
-                className="text-white w-6 h-6 flex-shrink-0 mr-4"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <span className="text-white title-font font-medium">
-                Authentic Cliche Forage
-              </span>
-            </div>
-          </div>
-          <div className="p-2 sm:w-1/2 w-full">
-            <div className="bg-secondary rounded flex p-4 h-full items-center">
-              <svg
-                fill="none"
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="3"
-                className="text-white w-6 h-6 flex-shrink-0 mr-4"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                />
-              </svg>
-              <span className="text-white title-font font-medium">
-                Authentic Cliche Forage
-              </span>
-            </div>
-          </div>
+        <div className="lg:w-2/3 w-full mx-auto overflow-auto">
+          <table className="table-auto w-full text-left whitespace-no-wrap">
+            <thead>
+              <tr>
+                <th className="px-4 py-3 title-font tracking-wider font-medium text-black text-sm bg-base-100 rounded-tl rounded-bl">
+                  Service
+                </th>
+                <th className="px-4 py-3 title-font tracking-wider font-medium text-black text-sm bg-base-100 rounded-tr rounded-br">
+                  Pricing
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="text-black bg-white px-4 py-3">
+                  Root Color
+                </td>
+                <td className="text-black bg-white px-4 py-3">
+                  $70 +
+                </td>
+              </tr>
+              <tr>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  Haircut
+                </td>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  $60 +
+                </td>
+              </tr>
+              <tr>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  Full Color
+                </td>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  $150 +
+                </td>
+              </tr>
+              <tr>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  Partial Balayage
+                </td>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  $150 +
+                </td>
+              </tr>
+              <tr>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  Balayage
+                </td>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  $210 +
+                </td>
+              </tr>
+              <tr>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  Partial Foil
+                </td>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  $85 +
+                </td>
+              </tr>
+              <tr>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  Full Foil
+                </td>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                 $125 +
+                </td>
+              </tr>
+              <tr>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  Full Babylight
+                </td>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  $250 +
+                </td>
+              </tr>
+              <tr>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  Full Bleach Out
+                </td>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  $300 +
+                </td>
+              </tr>
+              <tr>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  Bleach Root Touch Up (6 Weeks)
+                </td>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  $100 +
+                </td>
+              </tr>
+              <tr>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  Root Shadow
+                </td>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  $25 +
+                </td>
+              </tr>
+              <tr>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  For Additional Toners
+                </td>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  $30 - $60
+                </td>
+              </tr>
+              <tr>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  For Additional Vidids
+                </td>
+                <td className="text-black border-t-2 bg-white border-neutral px-4 py-3">
+                  $80 - $150
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </div>
-        <button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-          Button
-        </button>
+        <div className="flex pl-4 mt-4 lg:w-2/3 w-full mx-auto">
+          <button className="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
+            Button
+          </button>
+        </div>
       </div>
     </section>
   );
