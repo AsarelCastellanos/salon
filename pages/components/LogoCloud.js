6ebd080b425fed5234e4632a74bc89eeb90ab9c3
Asarel Castellanos <asarelc@gmail.com>
2022-04-11 23:21:46 -0700

    Added footer and testimonials
    I need to add and edit content in testimonials, logocloud , find us and footer.

--- pages/components/LogoCloud.js
+++ pages/components/LogoCloud.js
@@ -4,8 +4,8 @@ const LogoCloud = () => {
   return (
     <div className="bg-primary">
       <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-        <h2 className="text-center text-gray-400 text-sm font-semibold uppercase tracking-wide">
-          TTrusted Brands We Use
+        <h2 className="text-center text-black text-sm font-semibold uppercase tracking-wide">
+          Trusted Brands We Use
         </h2>
         <div className="mt-8 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
