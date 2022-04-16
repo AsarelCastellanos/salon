7fcd3136d5325d344094251b24690dde9a98cf1c
Asarel Castellanos <asarelc@gmail.com>
2022-04-16 11:34:52 -0700

    Updated where I store metatags
    I made its own metatags file. Testing if this will work.

--- pages/_app.js
+++ pages/_app.js
@@ -1,49 +1,7 @@
-import Head from "next/head";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        {/* <!-- Primary Meta Tags --> */}
-        <meta name="title" content="Vicious Streak Salon" />
-        <meta
-          name="description"
-          content="Vicious Streak Salon by Vidalia Arellano. I'm a hair vivids specialist and a hair extension specialist located in Santa Ana, California. Certified with Bellami Hair."
-        />
-        <meta name="author" content="Vidalia Arellano" />
-        {/* <!-- Open Graph / Facebook --> */}
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://www.viciousstreaksalon.com" />
-        <meta property="og:title" content="Vicious Streak Salon" />
-        <meta
-          property="og:description"
-          content="Vicious Streak Salon by Vidalia Arellano. I'm a hair vivids specialist and a hair extension specialist located in Santa Ana, California. Certified with Bellami Hair."
-        />
-        <meta
-          property="og:image"
-          content="/profile/salon.jpg"
-        />
-
-        {/* <!-- Twitter --> */}
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta
-          property="twitter:url"
-          content="https://www.viciousstreaksalon.com"
-        />
-        <meta property="twitter:title" content="Vicious Streak Salon" />
-        <meta
-          property="twitter:description"
-          content="Vicious Streak Salon by Vidalia Arellano. I'm a hair vivids specialist and a hair extension specialist located in Santa Ana, California. Certified with Bellami Hair."
-        />
-        <meta
-          property="twitter:image"
-          content="/profile/salon.jpg"
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return ( <Component {...pageProps} /> );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
 import React from "react"
+import MetaTags from "./meta";
 
 // Importing Main Components of Website
 import NavBar from "./components/NavBar"
 import Views from "./views"
 import Footer from "./components/Footer"
+import Head from "next/head"
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <MetaTags />
+      </Head>
       <NavBar />
       <Views />
       <Footer />
