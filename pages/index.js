8e0625c8775e67defa632010ce061c7d55f39b90
Asarel Castellanos <asarelc@gmail.com>
2022-04-24 01:18:18 -0700

    adding a filler testimonial array

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { groq } from "next-sanity";
 import { usePreviewSubscription, urlFor } from "../lib/sanity";
 import { getClient } from "../lib/sanity.server";
-import Image from "next/image";
 
 import Hero from "./components/Hero";
 import Services from "./components/Services";
@@ -25,7 +24,16 @@ const testimonialsQuery = groq`*[_type == "testimonials"]{
   'image':image.asset->url
 }`;
 
+const testimonials = [{
+  _id: '0001',
+  date: '01012001',
+  name: 'Testing...',
+  description: 'Testing...',
+  image: 'image'
+}];
+
 export default function Home({ data, preview }) {
+
   const { data: website } = usePreviewSubscription(websiteQuery, {
     initialData: data.website,
     enabled: preview,
@@ -42,49 +50,9 @@ export default function Home({ data, preview }) {
   return (
     <>
       <Hero title={title} description={description} image={image} />
-      <Services />
-      <Gallery
-        extensionDescription={extensionDescription}
-        vividDescription={vividDescription}
-      />
-      <section id="testimonials" className="bg-primary body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <h1 className="text-3xl font-medium title-font text-white mb-20 text-center">
-            Testimonials
-          </h1>
-          <div className="flex flex-wrap -m-4">
-            {testimonials && testimonials.map((testimonial) => {
-              return (
-                <div
-                  key={testimonial._id}
-                  className="lg:w-1/3 lg:mb-0 mb-6 p-4"
-                >
-                  <div className="h-full text-center">
-                    <div className="w-20 h-20 mb-8 object-cover object-center rounded-full inline-block border-2 border-white bg-gray-100">
-                      <Image
-                        className="rounded-full"
-                        src={testimonial.image}
-                        alt="Charles"
-                        width="10"
-                        height="10"
-                        layout="responsive"
-                      />
-                    </div>
-                    <p className="leading-relaxed text-white">
-                      {testimonial.description}
-                    </p>
-                    <span className="inline-block h-1 w-10 rounded bg-primary mt-2 mb-2"></span>
-                    <h2 className="text-white font-medium title-font tracking-wider text-sm">
-                      {testimonial.name}
-                    </h2>
-                    <p className="text-white">{testimonial.date}</p>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
-        </div>
-      </section>
+      <Services/>
+      <Gallery extensionDescription={extensionDescription} vividDescription={vividDescription}/>
+      <Testimonials testimonials={testimonials}/>
       <FindUs />
     </>
   );
@@ -93,12 +61,6 @@ export default function Home({ data, preview }) {
 export async function getStaticProps({ preview = true }) {
   const website = await getClient(preview).fetch(websiteQuery);
   const testimonials = await getClient(preview).fetch(testimonialsQuery);
-
-  if (!website || !testimonials) {
-    return {
-      notFound: true,
-    };
-  }
 
   return {
     props: {
