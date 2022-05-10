e9e42719cd83f986234ff6dce95b1555ca77bd76
Asarel Castellanos <asarelc@gmail.com>
2022-05-10 00:51:57 -0700

    updated typo on logocloud

--- pages/components/LogoCloud.js
+++ pages/components/LogoCloud.js
@@ -5,7 +5,7 @@ const LogoCloud = () => {
     <div className="bg-primary">
       <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
         <h2 className="text-center text-white text-md font-semibold uppercase tracking-wide">
-          Using on the best brands and products
+          Using only the best brands and products
         </h2>
         <div className="mt-8 grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
           <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
