866671e7ca5da7d08a80b4ef78804651eb680a58
Asarel Castellanos <asarelc@gmail.com>
2022-05-19 21:18:14 -0700

    css format update

--- pages/components/CTAGallery.js
+++ pages/components/CTAGallery.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 const CTAGallery = () => {
   return (
     <div className="bg-primary">
-      <div className="max-w-2xl mx-auto text-center py-16 px-4 sm:py-20 sm:px-6 lg:px-8">
+      <div className="max-w-2xl px-4 py-16 mx-auto text-center sm:py-20 sm:px-6 lg:px-8">
         <h2 className="text-2xl font-extrabold text-white sm:text-3xl">
           <span className="block">Unsure? Nervous?</span>
           <span className="block">Lost? Concerned?</span>
@@ -13,7 +13,7 @@ const CTAGallery = () => {
           That&apos;s fine! I know it can be difficult to trust someone to handle your hair. In order to help ease you I&apos;ve created a gallery that shows some of the past hair I&apos;ve worked on.
         </p>
         <Link href="/gallery" passHref>
-          <button className="btn bg-white text-primary hover:text-white capitalize mt-6 w-full text-lg font-medium rounded-md sm:w-auto">
+          <button className="w-full mt-6 text-lg font-medium capitalize bg-white rounded-md btn text-primary hover:text-white sm:w-auto">
             View Gallery
           </button>
         </Link>
